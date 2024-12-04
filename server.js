5db5cea906b339740e217bb85e389326e9ccd55e
parbhat-dhanuk <parbhatdhanuk3@gmail.com>
2024-12-04 16:35:46 +0545

    nodejs with redis

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from "express"
+import axios from "axios"
+import client from "./client.js"
+const app=express()
+
+
+app.get('/',async(req,res)=>{
+
+    const cacheValue=await client.get('todos')
+
+    if(cacheValue){
+     return res.json(JSON.parse(cacheValue))
+    }
+
+    const {data}= await axios.get("https://jsonplaceholder.typicode.com/todos")
+    await client.set("todos",JSON.stringify(data))
+    await client.expire("todos",30)
+    return res.json(data)
+
+    
+})
+
+
+app.listen(6000,()=>{
+    console.log('server is running')
+})
